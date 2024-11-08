e131534930ebaa49a6236ac898626b12f665242e
Vitor_Oficial_Tururu <jvitoralvernaz@outlook.com>
2024-11-08 15:57:02 -0400

    ATT_Test_3

--- a/Index_Script.js
+++ a/Index_Script.js
@@ -3,6 +3,7 @@ let money = Number(money_View.innerHTML);
 let click = 1;
 let prod1 = 1;
 let prod2 = 1;
+let prod3 = 0;
 
 function a(){
     money=money+click;
@@ -49,6 +50,39 @@ async function produto2(){
         prod2 = prod2 +1;
         click_Calculator();
     }
+}
+
+async function produto3(){
+
+    const qtd =document.getElementById("qtdProduto3")
+    const valor =document.getElementById("valorProduto3")
+
+    let qtd_Number = Number(qtd.innerHTML);
+    let valor_Number = Number(valor.innerHTML)
+    
+
+
+    if(valor_Number<= money){
+        qtd.innerHTML = qtd_Number+1;
+        money=money-valor_Number;
+        money_View.innerHTML = Math.trunc(money);
+        valor.innerHTML = 1000*Math.pow(10,qtd_Number);
+        prod3 = prod3 +1;
+        
+        if(prod3 = 0){
+            teste();
+        }
+    }
+}
+
+async function teste(){
+    setTimeout(function()
+    {
+        alert("a");
+        money=money+(prod3*click);
+        money_View.innerHTML = money;
+        teste()
+    },5000);
 }
 
 function click_Calculator(){
